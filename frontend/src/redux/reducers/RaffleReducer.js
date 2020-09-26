544e1c630b54e47d3ce25a3f797f0b816b0e27f9
Nádson <nadsonfernando1@gmail.com>
2020-09-26 18:44:06 -0300

    refactory some stuff

--- frontend/src/redux/reducers/RaffleReducer.js
+++ frontend/src/redux/reducers/RaffleReducer.js
@@ -1,22 +1,7 @@
 import ActionType from "@redux/actions";
 import RaffleService from "@services/http/RaffleService";
 
-const initialState = {
-  raffles: [],
-};
-
-async function onFindAll({ state, dispatch }) {
-  const { isSuccess, data } = await RaffleService.findAll();
-
-  if (isSuccess) {
-    dispatch({
-      type: ActionType.FETCH_RAFFLES_REDUCER_PERSIST,
-      raffles: data,
-    });
-  }
-
-  return state;
-}
+const initialState = {};
 
 async function onRafle({ state, dispatch, friends }) {
   const { isSuccess } = await RaffleService.raffle(friends);
@@ -29,16 +14,12 @@ async function onRafle({ state, dispatch, friends }) {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ActionType.FETCH_RAFFLES:
-      return onFindAll({ state, dispatch: action.dispatch });
     case ActionType.RAFFLE:
       return onRafle({
         state,
         dispatch: action.dispatch,
         friends: action.friends,
       });
-    case ActionType.FETCH_RAFFLES_REDUCER_PERSIST:
-      return { ...state, raffles: action.raffles };
     default:
       return state;
   }
--- frontend/src/services/http/RaffleService.js
+++ frontend/src/services/http/RaffleService.js
@@ -3,18 +3,6 @@ import ResponseUtil from "@utils/ResponseUtil";
 
 const resource = "/raffles";
 
-async function findAll() {
-  let response = {};
-
-  try {
-    response = await Api.get(resource);
-  } catch (error) {
-    response = error.response;
-  }
-
-  return ResponseUtil.filter(response);
-}
-
 async function raffle(friends) {
   let response = {};
 
@@ -27,4 +15,4 @@ async function raffle(friends) {
   return ResponseUtil.filter(response);
 }
 
-export default { findAll, raffle };
+export default { raffle };
