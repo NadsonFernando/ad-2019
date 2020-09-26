baa27489afcf7784bc6e93508b48100ed1eff0f3
Nádson <nadsonfernando1@gmail.com>
2020-09-26 20:57:56 -0300

    add sendgrid mail

--- backend/src/services/mail.js
+++ backend/src/services/mail.js
@@ -1,31 +1,18 @@
-import nodemailer from "nodemailer";
+import sendgrid from "@sendgrid/mail";
+
+sendgrid.setApiKey(
+  "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"
+);
 
 export default {
   send(to, name) {
-    const transporter = nodemailer.createTransport({
-      host: process.env.smtp,
-      port: process.env.mail_port,
-      secure: false,
-      auth: {
-        user: process.env.mail_user,
-        pass: process.env.mail_pass,
-      },
-      tls: { rejectUnauthorized: false },
-    });
-
-    const options = {
+    const message = {
       to,
       from: "nadsonfernando1@gmail.com",
       subject: "Amigo secreto!",
       text: `Você saiu com ${name}`,
     };
 
-    transporter.sendMail(options, function (error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email enviado: " + info.response);
-      }
-    });
+    sendgrid.send(message);
   },
 };
